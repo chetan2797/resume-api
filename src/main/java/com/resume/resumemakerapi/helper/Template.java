08f7f4881d1e0ea1fe8ed92a4d0820d9c14e0b75
Chetan Gupta <91193409+chetan2797@users.noreply.github.com>
2022-03-25 14:04:58 +0530

    Update Template.java

--- src/main/java/com/resume/resumemakerapi/helper/Template.java
+++ src/main/java/com/resume/resumemakerapi/helper/Template.java
@@ -4,13 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import com.resume.resumemakerapi.ResumeMakerApiApplication;
+
 import org.springframework.core.io.ClassPathResource;
 
 public class Template {
      
     static {
         try {
-            System.out.println(new ClassPathResource("").getFile());
+            ResumeMakerApiApplication obj = new ResumeMakerApiApplication();
+            
+            System.out.println(obj.getClass().getClassLoader().getResourceAsStream("arg0"));
             HEAD = new String(Files.readAllBytes(java.nio.file.Paths.get(new ClassPathResource("static").getFile().getAbsolutePath()+File.separator+"head-data.html")));
         } catch (IOException e) {
             e.printStackTrace();
