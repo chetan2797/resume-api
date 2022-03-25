f74aca3a2740adda41ef5d940d5636b5de2e6497
Chetan Gupta <91193409+chetan2797@users.noreply.github.com>
2022-03-25 15:25:38 +0530

    Update Template.java

--- src/main/java/com/resume/resumemakerapi/helper/Template.java
+++ src/main/java/com/resume/resumemakerapi/helper/Template.java
@@ -3,32 +3,14 @@ package com.resume.resumemakerapi.helper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-
-import com.resume.resumemakerapi.ResumeMakerApiApplication;
 
 import org.springframework.core.io.ClassPathResource;
 
 public class Template {
 
-    static { 
-        try{
-            ResumeMakerApiApplication obj = new ResumeMakerApiApplication();
-            ClassLoader classLoader = obj.getClass().getClassLoader();
-            File file = new File(classLoader.getResource("static"+File.separator+"head-data.html").getFile());
-            System.out.println(14);
-            HEAD = new String(Files.readAllBytes(java.nio.file.Paths.get(file.getAbsolutePath())));
-            System.out.println("yes");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try{
-            System.out.println(15);
-            byte[] byteArrays = Files.readAllBytes(
-                    Path.of(new ClassPathResource("static").getFile().getAbsolutePath()+File.separator+"head-data.html")
-            );
-            HEAD = new String(byteArrays);
-            System.out.println("yes");
+    static {
+        try {
+            HEAD = new String(Files.readAllBytes(java.nio.file.Paths.get(new ClassPathResource("static").getFile().getAbsolutePath()+File.separator+"head-data.html")));
         } catch (IOException e) {
             e.printStackTrace();
         }
