4322189bb3ae8882e5cf2d63b7ee4be65b8885bd
Chetan Gupta <91193409+chetan2797@users.noreply.github.com>
2022-03-25 14:06:31 +0530

    Update Template.java

--- src/main/java/com/resume/resumemakerapi/helper/Template.java
+++ src/main/java/com/resume/resumemakerapi/helper/Template.java
@@ -12,9 +12,6 @@ public class Template {
      
     static {
         try {
-            ResumeMakerApiApplication obj = new ResumeMakerApiApplication();
-            
-            System.out.println(obj.getClass().getClassLoader().getResourceAsStream("arg0"));
             HEAD = new String(Files.readAllBytes(java.nio.file.Paths.get(new ClassPathResource("static").getFile().getAbsolutePath()+File.separator+"head-data.html")));
         } catch (IOException e) {
             e.printStackTrace();
