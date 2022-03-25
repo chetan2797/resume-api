33f6fa67466a3eabd61fa22f1dccb6aadc5a0763
Chetan Gupta <91193409+chetan2797@users.noreply.github.com>
2022-03-25 14:54:25 +0530

    Update Template.java

--- src/main/java/com/resume/resumemakerapi/helper/Template.java
+++ src/main/java/com/resume/resumemakerapi/helper/Template.java
@@ -4,26 +4,27 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import com.resume.resumemakerapi.ResumeMakerApiApplication;
-
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.ResourceUtils;
 
 public class Template {
 
     static {
+        try{System.out.println(new ClassPathResource("").getInputStream());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:static/head-data.html").getAbsolutePath());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:static"));}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:static").toPath());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:").toPath().toAbsolutePath());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:").toPath().toFile());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:").exists());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:static").toPath().toAbsolutePath());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:static").toPath().toUri());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:static").toPath().toString());}catch(Exception e){}
+        try{System.out.println(ResourceUtils.getFile("classpath:static").toPath().toRealPath());}catch(Exception e){}
         try {
-            ResumeMakerApiApplication obj = new ResumeMakerApiApplication();
-            
-            System.out.println(obj.getClass().getClassLoader().getResourceAsStream(""));
-            try{System.out.println(new ClassPathResource("").getInputStream());}catch(Exception e){}
-            try{System.out.println(ResourceUtils.getFile("classpath:static/head-data.html").getAbsolutePath());}catch(Exception e){}
-            try{System.out.println(ResourceUtils.getFile("classpath:static"));}catch(Exception e){}
-            try{System.out.println(ResourceUtils.getFile("classpath:static").toPath());}catch(Exception e){}
-
             HEAD = new String(Files.readAllBytes(java.nio.file.Paths.get(new ClassPathResource("static").getFile().getAbsolutePath()+File.separator+"head-data.html")));
+            System.out.println("yes");
         } catch (IOException e) {
-            e.printStackTrace();
         }
     }
     
